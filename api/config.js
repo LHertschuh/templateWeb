f8a68a07ccdb6458c84925531df10015a0d48ec0
Louis Hertschuh <louishertschuh@MacBook-Air-de-Louis.local>
2025-10-30 16:04:32 +0100

    Modification credentials

--- api/config.js
+++ api/config.js
@@ -1,11 +1,11 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-d40798bipnbc73cf4njg-a.oregon-postgres.render.com",
+    "host" :"dpg-d41mvefgi27c739m6lr0-a.oregon-postgres.render.com",
     "port" : "5432",
-    "user" : "cnam_qnjc_user",
-    "password" : "FOYjmLxOmqwdnfGPhMO4R8eZpErPzVXQ",
-    "bdname" :"cnam_qnjc" 
+    "user" : "pollution_5fsv_user",
+    "password" : "iW1Xm4vz7XNEd9xiqhoDDX9AUCSsvXJl",
+    "bdname" :"pollution_5fsv" 
     }
 }
 
